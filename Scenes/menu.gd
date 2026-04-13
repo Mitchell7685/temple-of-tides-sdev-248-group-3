7fb79560da55d855ea1575ca1a86c956253fcd0a
Mitchell Kehler <kehlermitchell@gmail.com>
2026-04-13 09:51:48 -0400

    updates

--- Scenes/menu.gd
+++ Scenes/menu.gd
@@ -4,4 +4,4 @@ extends Control
 
 func _input(event):
 	if event.is_action_pressed("ui_accept"):
-		get_tree().change_scene_to_file("res://MainGame.tscn")
+		get_tree().change_scene_to_file("res://Scenes/world_1.tscn")
