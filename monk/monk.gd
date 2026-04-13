20fbe2bfac66e89fcbed3489e14aa3e4591a1b7d
Mitchell Kehler <kehlermitchell@gmail.com>
2026-04-13 17:31:38 -0400

    update to last

--- monk/monk.gd
+++ monk/monk.gd
@@ -8,5 +8,10 @@ func talk(viewport: Node, event: Input, _shape_id: int):
 	if event.is_action_just_pressed("interact"):
 		talking.emit(message)
 
+#When interacted, show text
 func interact():
 	print("monk interacted")
+	$Label.show()
+	await get_tree().create_timer(4.0).timeout
+	$Label.hide()
+	print("interact ended")
